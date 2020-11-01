a9719c285c891ba8fd819e53d177321f0c772b5b
anderson-cahet <anderson.cahet@ghe.megaleo.com>
2020-11-01 14:47:28 +0000

    feat: header+form
    add header and a form to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
-
+import { PageHeader } from '@workday/canvas-kit-react-page-header';
+import TextInput from '@workday/canvas-kit-react-text-input';
+import FormField from '@workday/canvas-kit-react-form-field';
+import { Button } from '@workday/canvas-kit-react-button';
+import { useState } from 'react';
 function App() {
-	return <div className="App">Start</div>;
+  
+	return (
+		<div className="app">
+			<PageHeader title="ToDo APP" />
+			<FormField
+				label="My Field"
+				inputId="my-input-field"
+				hintText="Please add a todo"
+				hintId="my-input-field-error"
+				error={FormField.ErrorType.Error}
+			>
+				<TextInput placeholder="Placeholder" value="value" onChange={() => {}} />
+				<Button size={Button.Size.Small}>Small Button</Button>
+			</FormField>
+			
+		</div>
+	);
 }
 
 export default App;
